91ec85d82e5b929b257f56b1d5035a4fcc6ab5c3
DESKTOP-SQHQBRG\Jewel Rose <mjrnlorejas@tip.edu.ph>
2022-09-12 18:35:58 +0800

    update

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,66 +1,45 @@
 <template>
   <v-card
     class="mx-auto"
-    max-width="344"
+    color="#26c6da"
+    dark
+    max-width="400"
   >
-  <v-img
-  lazy-src="https://scontent.fmnl8-3.fna.fbcdn.net/v/t1.15752-9/305383537_1131372360791853_1542495798935658143_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeH6BkTr-gKJrUpTfvqwo0knjqOaQm_fzCCOo5pCb9_MIKvS-77VQIAYqCsbXe1ezFqnb6WeSDk81KV03JWcyBF1&_nc_ohc=3D6hWZmIMzkAX8km9CF&_nc_ht=scontent.fmnl8-3.fna&oh=03_AVJqX3VD_mHYfbMYjjUnGVlSlajr3E62FgAapXc9AiNq6g&oe=63424BC3"
-  max-height="350"
-  max-width="250"
-  src="https://scontent.fmnl8-3.fna.fbcdn.net/v/t1.15752-9/305383537_1131372360791853_1542495798935658143_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeH6BkTr-gKJrUpTfvqwo0knjqOaQm_fzCCOo5pCb9_MIKvS-77VQIAYqCsbXe1ezFqnb6WeSDk81KV03JWcyBF1&_nc_ohc=3D6hWZmIMzkAX8km9CF&_nc_ht=scontent.fmnl8-3.fna&oh=03_AVJqX3VD_mHYfbMYjjUnGVlSlajr3E62FgAapXc9AiNq6g&oe=63424BC3"
-></v-img>
-
     <v-card-title>
-        Hello & Welcome to my page!
+      <v-icon
+        large
+        left
+      >
+        mdi-facebook
+      </v-icon>
+      <span class="text-h6 font-weight-light">My Profile</span>
     </v-card-title>
 
-    <v-card-subtitle>
-        My name is Jewel Rose Lorejas. 
-    </v-card-subtitle>
+    <v-card-text class="text-h5 font-weight-bold">
+      Hello & Welcome to my page!
+    </v-card-text>
 
     <v-card-actions>
-      <v-btn
-        color="orange lighten-2"
-        text
-      >
-        About Me:
-      </v-btn>
+      <v-list-item class="grow">
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            alt=""
+            src="https://scontent.fmnl8-3.fna.fbcdn.net/v/t1.15752-9/305383537_1131372360791853_1542495798935658143_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeH6BkTr-gKJrUpTfvqwo0knjqOaQm_fzCCOo5pCb9_MIKvS-77VQIAYqCsbXe1ezFqnb6WeSDk81KV03JWcyBF1&_nc_ohc=3D6hWZmIMzkAX8km9CF&_nc_ht=scontent.fmnl8-3.fna&oh=03_AVJqX3VD_mHYfbMYjjUnGVlSlajr3E62FgAapXc9AiNq6g&oe=63424BC3"
+          ></v-img>
+        </v-list-item-avatar>
 
-      <v-spacer></v-spacer>
+        <v-list-item-content>
+          <v-list-item-title>JEWEL ROSE LOREJAS</v-list-item-title>
+        </v-list-item-content>
 
-      <v-btn
-        icon
-        @click="show = !show"
-      >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
+        <v-row
+          align="center"
+          justify="end"
+        >
+          <span class="subheading">45</span>
+        </v-row>
+      </v-list-item>
     </v-card-actions>
-
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
-
-        <v-card-text>
-            I am a 4th year college studying Bachelor of Scinece in Information Technology 
-        at Technological Institute of the Philippines. 
-        </v-card-text>
-        <v-card-text>
-            I am a simple girl who enjoys keeping life simple, and I am a kind-hearted person. 
-            As I get older, I dream of living independently. 
-            I want to be in loving relationships, but I don't want to be dependent on anyone. 
-            I want to be financially and emotionally self-sufficient and strong. 
-            I enjoy learning and trying new things in life. I am grateful for what I have and want to do 
-            everything that I can to make others happy. 
-        </v-card-text>
-      </div>
-    </v-expand-transition>
   </v-card>
 </template>
-
-<script>
-    export default {
-      data: () => ({
-        show: false,
-      }),
-    }
-  </script>
--- src/views/AboutVuetify.vue
+++ src/views/AboutVuetify.vue
@@ -1,22 +1,22 @@
 <template>
-<v-card
-       class="ma-15"
-       max-width="650"
-    >
-    <v-row justify="center">
-    <v-img
-      src="https://pbs.twimg.com/media/Ei5n6vBWoAEy5gp.png"
-      lazy-src="https://pbs.twimg.com/media/Ei5n6vBWoAEy5gp.png"
-      max-width="400"
-      max-height="300"
- >
- </v-img>
-
-    <v-card-title> <h1 class="text-center ma-8">Vuetify</h1></v-card-title>
+   <v-parallax
+     dark
+     src="https://a.storyblok.com/f/87002/1280x720/c23d81e2d2/escuela-vue-curso-vue-router.jpg"
+   >
+     <v-row
+       align="center"
+       justify="center"
+     >
+       <v-col
+         class="text-center"
+         cols="12"
+       >
+         <v-card-title> <h1 class="text-center ma-8">Vuetify</h1></v-card-title>
       <h3 class="text-center ma-10">Vuetify is a Vue UI Library with beautifully handcrafted Material Components. No design skills required — everything you need to create amazing applications is at your fingertips.</h3>
    
       <h4 class="text-center ma-10">Link: <a href="https://vuetifyjs.com/en/">https://vuetifyjs.com/en/</a></h4>
-   </v-row>
-    </v-card>  
-</template>
-   
+
+       </v-col>
+     </v-row>
+   </v-parallax>
+ </template>
